db7f2c211c7053e1330155ac63e0714f6bb9a38d
Sanskar Gupta <sanskaargupta050@gmail.com>
2025-05-31 17:28:47 +0530

    reset

--- script.js
+++ script.js
@@ -19,12 +19,8 @@ function next() {
         slideImg()
     }
     else if (counter === (slides.length - 1)) {
-        document.querySelector(".next").innerHTML = "Reset"
         counter = 0
         slideImg()
-        setTimeout(() => {
-            document.querySelector(".next").innerHTML = "Next"
-        }, 1000);
     }
 }
 
